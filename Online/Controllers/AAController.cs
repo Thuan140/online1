626236d26d6909f4ab492a5d78297b6823df9120
Thuan140 <phamngocthuan2005@gmail.com>
2024-10-28 17:23:50 +0700

    Online1

---
+++ Online/Controllers/AAController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online.Controllers
+{
+    public class AAController : Controller
+    {
+        public ActionResult AncientArchitecture()
+        {
+            return View();
+        }
+    }
+}
---
+++ Online/Controllers/PSEController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online.Controllers
+{
+    public class PSEController : Controller
+    {
+        public ActionResult PhotoShootEffect()
+        {
+            return View();
+        }
+    }
+}
